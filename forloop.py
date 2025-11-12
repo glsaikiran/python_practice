4da7d3c548cf780f364c044fb3d38db3cc61cabf
glsaikiran <216837351+glsaikiran@users.noreply.github.com>
2025-11-12 15:05:57 +0530

    Python Practice Codes

---
+++ forloop.py
@@ -0,0 +1,8 @@
+for i in [5,4,3,2,1]:
+  print(i)
+print('Blastoff!')
+
+#next example
+friends =["sai","kiran","sru"]
+for i in friends:
+    print('Hi',i)
---
+++ forloopsmallest.py
@@ -0,0 +1,9 @@
+a= [100,30,6,1,9,5,10]
+b= None
+for i in a:
+    if b==None :
+      b=i
+    elif b>i:
+      b=i
+    print(b)
+print('Blastoff!')
