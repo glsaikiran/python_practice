ad9740fec8b7e6b551f7b4c8d73de79515016d0f
glsaikiran <216837351+glsaikiran@users.noreply.github.com>
2025-11-15 17:01:57 +0530

    upto date practice on 15.11.2025

--- nestedlist.py
+++ nestedlist.py
@@ -1,19 +1,21 @@
 for i in range(int(input())):
-        name = input()
-        score = float(input())
-        list1=[]
-        list2=[]
-        list3=[]
-        list4=[]
-        list1.append([name,score])
-        list2.append(score)
+  name = input()
+  score = float(input())
+  list1=[]
+  list2=[]
+  list3=[]
+  list4=[]
+  list1.append([name,score])
+  list2.append(score)
+print(list2)
 for i in list1:
-    if i not in list3:
-        list3.append(i)
+  if i not in list3:
+    print(list3)
+    list3.append(i)
 sec_low=list3[-2]
 for i in list1:
-    if i[1]==sec_low:
-        list4.append(i[0])
+  if i[1]==sec_low:
+    list4.append(i[0])
 list4.sort()
 for i in list4:
-    print(i)
+  print(i)
