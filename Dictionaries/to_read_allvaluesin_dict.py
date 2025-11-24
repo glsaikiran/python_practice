49ddd2265fdc46a2d7ed3c3d941fc147961e3e12
glsaikiran <216837351+glsaikiran@users.noreply.github.com>
2025-11-24 15:11:36 +0530

    get func,read a dict words,two iteration variables

---
+++ Dictionaries/to_read_allvaluesin_dict.py
@@ -0,0 +1,7 @@
+counts={'sa':1,'sr':42,'ju':100}
+for key in counts:
+    print(key,counts[key])
+print(list(counts))
+print(counts.keys())
+print(counts.values())
+print(counts.items())  #This converts dict to tuple pairs.
---
+++ Dictionaries/two_iteration_variables.py
@@ -0,0 +1,3 @@
+counts={'sa':1,'sr':42,'ju':100}
+for i,j in counts.items():
+    print(i,j)
