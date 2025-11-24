ce0d8228974ff8a81e8d71f16651be9aa17c4bc3
glsaikiran <216837351+glsaikiran@users.noreply.github.com>
2025-11-24 15:49:39 +0530

    count of word is calculated

--- Dictionaries/countwords_in_file.py
+++ Dictionaries/countwords_in_file.py
@@ -6,3 +6,11 @@ for line in fhandle:
     for word in words:
         counts[word]=counts.get(word,0)+1
 print(counts)
+#Until above only gets count of words in a file ,now below we go to the dictionary and print largest count word
+bigword=None
+bigcount=None
+for key,value in counts.items():
+    if bigcount is None or value > bigcount:
+        bigword=key
+        bigcount=value
+print("Largest counted word: ",bigword," ",bigcount)
