cce892b98e12afaf098bbfc92db02277ae06c450
glsaikiran <216837351+glsaikiran@users.noreply.github.com>
2025-12-10 20:58:12 +0530

    reg exp example4,5

--- Hp_Pc_Python/ch11RegularExpressions/eg3.py
+++ Hp_Pc_Python/ch11RegularExpressions/eg3.py
@@ -2,5 +2,13 @@
 #extract only numbers from string
 import re
 x='My 2 favorite numbers are 19 and 42'
-y=re.findall('[0-9]+',x)
+y=re.findall(r'\d+',x)
 print(y)
+#if r is not kept in regular expre.Python handles backslashes in strings
+#before passing them to regex.​
+#The Problem Step-by-Step
+#You wrote: "\d+" (normal string)
+#Python sees \d and thinks "is this an escape like \n (new line)" → but its not found
+# so SyntaxWarning
+#Regex engine gets d+ only (backslash lost) → No digit matching! gives empty output
+#if you keep r it takes raw string not removing backlash to regex.
