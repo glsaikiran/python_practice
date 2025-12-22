b11fb60011ba12c36a3006c5bf3107f39a1d0893
glsaikiran <216837351+glsaikiran@users.noreply.github.com>
2025-12-22 17:13:13 +0530

    removed hp pc folder and kept all files directly in hppc branch

--- Hp_Pc_Python/ch10_Tuples/2.py
+++
@@ -1,3 +0,0 @@
-#tuples are immutable
-y=(1,7,2)
-y[0]=10
--- Hp_Pc_Python/ch10_Tuples/3.py
+++
@@ -1,4 +0,0 @@
-#Tuple can be kept on lefthand sid of a assignment statement,so that multiple variables values can be assignment as per their order
-(x,y)=(4,'fred')
-print(y)
-print(x)
